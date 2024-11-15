ba83ae754404b45f625310cc1dca27516e74b95d
harsh-choudhary-nature <harshchy2210@gmail.com>
2024-11-15 10:57:37 +0530

    made data.js correctly instructed

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const projectsData = [
 	{
     id: 1,
     title: 'Platformer - Search',
-    description: 'Students implement depth-first, breadth-first, uniform cost, and A* search algorithms. These algorithms are used to solve navigation and traveling salesman problems in the Platform world.',
+    description: 'Students are tasked to implement basic graph traversal techniques like depth first search, breadth first search, uniform cost search and A* search. These help them understand Travelling Salesman Problem in a game environment.',
     imageUrl: '/platformers-search/platformers-search.png',
     gifUrl: '/platformers-search/platformers-search.gif',
     zipUrl: '/platformers-search/platformers-search.zip',
@@ -10,7 +10,7 @@ export const projectsData = [
   {
     id: 2,
     title: 'Platformer - Multiagent',
-    description: 'Platformer game can be represented as both an adversarial and a probabilistic search problem. Students are tasked with implementing multi-agent minimax and expectimax algorithms while also creating evaluation functions, to combat the ghosts.',
+    description: 'Students are tasked with implementing multi-agent minimax and expectimax algorithms while also creating evaluation functions, to combat the ghosts. They also design interesting evaluation functions.',
     imageUrl: '/platformers-multiagent/platformers-multiagent.png',
     gifUrl: '/platformers-multiagent/platformers-multiagent.gif',
     zipUrl: '/platformers-multiagent/platformers-multiagent.zip',
