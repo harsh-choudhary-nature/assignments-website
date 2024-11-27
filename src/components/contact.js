3925d765f51bc47bb4c305d8071abfbbc436f3a3
harsh-choudhary-nature <harshchy2210@gmail.com>
2024-11-27 11:59:09 +0530

    added iitgoa email to contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,8 +7,7 @@ const Contact = () => {
       <h2>Contact</h2>
       <p>If you have any questions, feedback, or would just like to get in touch, feel free to reach out to us. We’re here to help and would love to hear from you.</p>
       <div className="contact-details">
-        <p><strong>Phone:</strong> <a href="tel:+1234567890" className="contact-link"> +123-456-7890</a></p>
-        <p><strong>Email:</strong> <a href="mailto:contact@example.com?subject=Inquiry%20from%20Website" className="contact-link"> contact@example.com</a></p>
+        <p><strong>Email:</strong> <a href="mailto:harsh.choudhary.21031@iitgoa.ac.in?subject=Inquiry%20from%20CS330%20Website" className="contact-link"> harsh.choudhary.21031@iitgoa.ac.in</a></p>
       </div>
     </section>
   );
